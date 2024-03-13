e2d84934d558979452b7d130a9104d8aa0602a35
wisdomstar94 <wisdomstar94@gmail.com>
2024-03-13 15:46:43 +0900

    invoke test

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,6 +1,8 @@
 #[tauri::command]
-pub fn greet(name: &str, age: u32) {
+pub fn greet(name: &str, age: u32) -> String {
   println!("greet fn called!");
   println!("name is {:#?}", name);
   println!("age is {:#?}", age);
+
+  format!("Hello, {}, {}!", name, age)
 }
--- src/app/test/invoke-handler/page.tsx
+++ src/app/test/invoke-handler/page.tsx
@@ -1,9 +1,27 @@
 "use client"
 
+import { invoke } from "@tauri-apps/api/core";
+import { MouseEvent } from "react";
+
 export default function Page() {
+  function callGreetFnInRust(event: MouseEvent<HTMLButtonElement>) {
+    console.log('@callGreetFnInRust', event);
+    invoke<string>('greet', { name: 'Next.js', age: 31 })
+      .then(result => {
+        console.log('@result', result);
+      })
+      .catch(console.error)
+    ;
+  }
+
   return (
     <>
-      
+      <button 
+        className="inline-flex border border-blue-400 text-blue-500 hover:bg-gray-50 px-3 py-1"
+        onClick={callGreetFnInRust}
+        >
+        rust 에 정의된 greet 함수 호출하기
+      </button>
     </>
   );
 }
